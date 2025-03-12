55f216824a4f45a5b350146e9d81805e50875219
Jakub Szewczyk <jakub.szewczyk@hotmail.com>
2025-03-12 13:57:52 +0100

    Split test to smaller ones

--- cypress/e2e/completeTodo.cy.js
+++ cypress/e2e/completeTodo.cy.js
@@ -1,13 +1,27 @@
 /// <reference types='cypress' />
 import { userSteps } from "../support/steps/userSteps"
 describe("Complete todo feature", () => {
-  it.only('User can complete a todo', () => {
+  it('User can complete a todo', () => {
     const todoName = "Zakonczone zadanie"
     userSteps.userOpensTodoMVCapp()
     userSteps.userCreatesANewTodo(todoName)
     userSteps.userCompletesTodo()
     userSteps.userChecksIfTodoMarkedAsCompleted()
+  })
+  
+  it('User can filter out Active todos', () => {
+    const todoName = "Zakonczone zadanie"
+    userSteps.userOpensTodoMVCapp()
+    userSteps.userCreatesANewTodo(todoName)
+    userSteps.userCompletesTodo()
     userSteps.userChecksIfCompletedTodoInNOTOnActiveList(todoName)
+  })
+
+  it('USer can filter Completed todos', () => {
+    const todoName = "Zakonczone zadanie"
+    userSteps.userOpensTodoMVCapp()
+    userSteps.userCreatesANewTodo(todoName)
+    userSteps.userCompletesTodo()
     userSteps.userChecksIfCompletedTodoIsOnCompletedList(todoName)
-  })  
-} )
+  })
+})
