6cd381e2d73101af21dd40ffc93bd52b73b38373
Jakub Szewczyk <jakub.szewczyk@hotmail.com>
2025-03-12 10:56:44 +0100

    User can create a todo - refactor

--- cypress/e2e/createTodo.cy.js
+++ cypress/e2e/createTodo.cy.js
@@ -1,8 +1,27 @@
 describe('Create todo feature', () => {
   it('User can create a todo', () => {
     const todoName = "Moje zadanie"
-    cy.visit('https://todomvc.com/examples/jquery/dist/#/all')
-    cy.get('#new-todo').type(todoName + '{enter}')
-    cy.get('#todo-list label').should('hava.text', todoName)
+    userOpensTodoMVCapp()
+    userCreatesANewTodo(todoName)
+    userChecksTodoIsCreated(todoName)
   })
 })
+
+function userOpensTodoMVCapp() {
+  cy.visit('https://todomvc.com/examples/jquery/dist/#/all')
+}
+
+function userCreatesANewTodo(todoName) {
+  cy.get('#new-todo').type(todoName + '{enter}')
+}
+
+function userChecksTodoIsCreated(todoName) {
+  //bardziej robudowany selektor, ale prostszy kod
+  cy.get('#todo-list label').should('have.text', todoName)
+
+  //jeżeli chcecie mieć krótsze selektory, ale bardziej skomplikowany kod
+  cy.get('#todo-list').then( ($todoList) => {
+    cy.wrap($todoList.text().trim()).should('be.eql', todoName)
+  })
+}
+
