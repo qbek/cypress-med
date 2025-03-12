0db15031b62463e4697f7bf40f20a0f7ec210638
Jakub Szewczyk <jakub.szewczyk@hotmail.com>
2025-03-12 12:05:32 +0100

    User can complete todo

--- cypress/e2e/createTodo.cy.js
+++ cypress/e2e/createTodo.cy.js
@@ -1,9 +1,20 @@
+/// <reference types='cypress' />
 describe('Create todo feature', () => {
   it('User can create a todo', () => {
     const todoName = "Moje zadanie"
     userOpensTodoMVCapp()
     userCreatesANewTodo(todoName)
     userChecksTodoIsCreated(todoName)
+  })
+
+  it.only('User can complete a todo', () => {
+    const todoName = "Zakonczone zadanie"
+    userOpensTodoMVCapp()
+    userCreatesANewTodo(todoName)
+    userCompletesTodo()
+    userChecksIfTodoMarkedAsCompleted()
+    userChecksIfCompletedTodoInNOTOnActiveList(todoName)
+    userChecksIfCompletedTodoIsOnCompletedList(todoName)
   })
 })
 
@@ -20,6 +31,24 @@ function userChecksTodoIsCreated(todoName) {
   cy.get('#todo-list label').should('have.text', todoName)
 
   //jeżeli chcecie mieć krótsze selektory, ale bardziej skomplikowany kod
+  cy.get('#todo-list').then( ($todoList) => {
+    cy.wrap($todoList.text().trim()).should('be.eql', todoName)
+  })
+}
+function userCompletesTodo() {
+  cy.get('.toggle').check()
+}
+function userChecksIfTodoMarkedAsCompleted() {
+  cy.get('#todo-list li').should('have.class', 'completed')
+}
+function userChecksIfCompletedTodoInNOTOnActiveList(todoName) {
+  cy.get('#filters [href="#/active"]').click()
+  cy.get('#todo-list').then( ($todoList) => {
+    cy.wrap($todoList.text().trim()).should('not.eql', todoName)
+  })
+}
+function userChecksIfCompletedTodoIsOnCompletedList(todoName) {
+  cy.get('#filters [href="#/completed"]').click()
   cy.get('#todo-list').then( ($todoList) => {
     cy.wrap($todoList.text().trim()).should('be.eql', todoName)
   })
