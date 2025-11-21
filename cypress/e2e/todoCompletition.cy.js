7b9d7910356f6467b580f3a35399d2e86a25015e
Jakub Szewczyk <jakub.szewczyk@hotmail.com>
2025-11-21 09:32:15 +0100

    Dodanie warstwy precondition

--- cypress/e2e/todoCompletition.cy.js
+++ cypress/e2e/todoCompletition.cy.js
@@ -3,6 +3,9 @@ import { newTodoInput } from "../support/pageobjects/newTodoInput"
 import { todoList } from "../support/pageobjects/todoList"
 import { todoFilters } from "../support/pageobjects/todoFilters"
 import { todoMVCApp } from "../support/pageobjects/todoMVCApp"
+import { preconditions } from "../support/actions/Preconditions"
+
+
 
 
 describe('Test suite to verify todo completiotion', () => {
@@ -13,26 +16,33 @@ describe('Test suite to verify todo completiotion', () => {
 
   it('User can complete a todo', () => {
     let todoToComplete = 'To zadanie trzeba zrobić'
-    newTodoInput.enterTodoName(todoToComplete)
-    newTodoInput.submitTodo()
+    preconditions.todoIsCreated
     todoList.completeCreatedTodo()
     todoList.checkTodoMarkedAsCompleted()
   })
 
   it('User can filter active todos', () => {
     let todoToComplete = 'To zadanie nie jest active'
+    //given
     newTodoInput.enterTodoName(todoToComplete)
     newTodoInput.submitTodo()
     todoList.completeCreatedTodo()
+    
+    //when
     todoFilters.gotoActive()
+
+    //then
     todoList.checkIsEmpty()
   })
 
   it('User can filter completed todos', () => {
     let todoToComplete = 'To zadanie jest completed'
+    
     newTodoInput.enterTodoName(todoToComplete)
     newTodoInput.submitTodo()
     todoList.completeCreatedTodo()
+
+
     todoFilters.gotoCompleted()
     todoList.checkTodoExists(todoToComplete)
   })
