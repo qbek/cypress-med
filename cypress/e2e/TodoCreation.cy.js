f07cde71d0bcbfbb00fb3529f0a6f8a2ca9255b3
Jakub Szewczyk <mrqbek@gmail.com>
2026-06-11 11:25:08 +0200

    Scenario splited into smaller ones

--- cypress/e2e/TodoCreation.cy.js
+++ cypress/e2e/TodoCreation.cy.js
@@ -16,12 +16,25 @@ describe('Todo creation feature tests', () => {
         cy.visit(TODOMVC_URL)
         cy.get('#new-todo').type(todoName + '{enter}')
         cy.get('.toggle').check()
-        cy.get('#todo-list li').should('have.attr', 'class', ' completed')
-        cy.get('#todo-list li').should('have.class', 'completed')
 
+        // cy.get('#todo-list li').should('have.attr', 'class', ' completed')
+        cy.get('#todo-list li').should('have.class', 'completed')
+    })
+
+    it('User can filter out completed todos from Active filter', () => {
+        const todoName = 'Completed todo for filtering'
+        cy.visit(TODOMVC_URL)
+        cy.get('#new-todo').type(todoName + '{enter}')
+        cy.get('.toggle').check()
         cy.get('#filters [href="#/active"]').click()
         cy.get('#todo-list').should('not.contain.text', todoName)
+    })
 
+    it('User can filter Completed todos', () => {
+        const todoName = 'Completed todo for another filterin'
+        cy.visit(TODOMVC_URL)
+        cy.get('#new-todo').type(todoName + '{enter}')
+        cy.get('.toggle').check()
         cy.get('#filters [href="#/completed"]').click()
         cy.get('#todo-list label').should('have.text', todoName)
     })
