525292a504f24b34d82024c4625c7e5fe398e56d
Jakub Szewczyk <mrqbek@gmail.com>
2026-06-11 10:40:38 +0200

    Scenario: User can complete a todo

--- cypress/e2e/TodoCreation.cy.js
+++ cypress/e2e/TodoCreation.cy.js
@@ -10,4 +10,19 @@ describe('Todo creation feature tests', () => {
         cy.get('#new-todo').type(todoName + '{enter}')
         cy.get('#todo-list label').should('have.text', todoName)  
     })
+
+    it('User can complete a todo', () => {
+        const todoName = 'Completed todo'
+        cy.visit(TODOMVC_URL)
+        cy.get('#new-todo').type(todoName + '{enter}')
+        cy.get('.toggle').check()
+        cy.get('#todo-list li').should('have.attr', 'class', ' completed')
+        cy.get('#todo-list li').should('have.class', 'completed')
+
+        cy.get('#filters [href="#/active"]').click()
+        cy.get('#todo-list').should('not.contain.text', todoName)
+
+        cy.get('#filters [href="#/completed"]').click()
+        cy.get('#todo-list label').should('have.text', todoName)
+    })
 })
